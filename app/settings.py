59209544dc0051e2213543ded00e5eec194b906c
Siphelele Miya <siphelelekunene2001@gmail.com>
2024-04-25 17:45:46 +0200

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -225,7 +225,7 @@ AUTH_COOKIE_SAMESITE = 'None'
 
 CORS_ALLOWED_ORIGINS = getenv(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:3000,http://127.0.0.1:3000'
+    'http://localhost:3000,http://127.0.0.1:3000,https://tut-view-labs-system.vercel.app'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
