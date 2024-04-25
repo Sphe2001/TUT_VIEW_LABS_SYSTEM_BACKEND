256486c77e4dc811d0dd8f4fef4375d9fc750c33
Siphelele Miya <siphelelekunene2001@gmail.com>
2024-04-25 07:12:18 +0200

    removed vercel config

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 DEBUG = getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS',
-                       '127.0.0.1,localhost,.vercel.app').split(',')
+                       '127.0.0.1,localhost').split(',')
 
 
 # Application definition
